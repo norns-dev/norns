9d49e29dd27e973bfc597517fb0471f6bed5cc77
Josh Odell <j01101111sh@gmail.com>
2024-02-06 15:44:31 -0600

    Added list view for parties

--- parties/urls.py
+++ parties/urls.py
@@ -2,6 +2,9 @@
 
 from django.urls import path
 
-from .views import PartyDetailView
+from .views import PartyDetailView, PartyListView
 
-urlpatterns = [path("<int:pk>/", PartyDetailView.as_view(), name="party_detail")]
+urlpatterns = [
+    path("<int:pk>/", PartyDetailView.as_view(), name="party_detail"),
+    path("", PartyListView.as_view(), name="party_list"),
+]
