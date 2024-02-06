99289db407e05066ab3cb8f051f0fcddeafc2cec
Josh Odell <j01101111sh@gmail.com>
2024-02-06 15:04:25 -0600

    Created parties models and party detail view

--- parties/views.py
+++ parties/views.py
@@ -1,1 +1,27 @@
 """Views for parties app"""
+
+from django.views import View
+from django.views.generic import DetailView
+
+from .models import Party
+
+
+class MemberGet(DetailView):
+    """Returns member for party"""
+
+    model = Party
+    template_name = "party_detail.html"
+
+
+class PartyDetailView(View):
+    """View for party details"""
+
+    def get(self, request, *args, **kwargs):
+        """Returns details of party for GET methods"""
+        view = MemberGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        """Returns details of party for POST methods"""
+        view = MemberGet.as_view()
+        return view(request, *args, **kwargs)
