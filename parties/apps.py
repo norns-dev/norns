cb95226c2089a4f0b1248102462ae9369957bcba
Josh Odell <j01101111sh@gmail.com>
2024-02-06 12:52:00 -0600

    Initial setup for parties app

---
+++ parties/apps.py
@@ -0,0 +1,10 @@
+"""App config for parties app"""
+
+from django.apps import AppConfig
+
+
+class PartiesConfig(AppConfig):
+    """App config for parties app"""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "parties"
