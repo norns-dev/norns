1a0ea2741a9612483dc16d7968604d76aaacc72c
Josh Odell <j01101111sh@gmail.com>
2024-02-05 23:30:10 -0600

    Initial accounts app setup

---
+++ accounts/apps.py
@@ -0,0 +1,10 @@
+"""accounts apps"""
+
+from django.apps import AppConfig
+
+
+class AccountsConfig(AppConfig):
+    """Accounts config"""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "accounts"
