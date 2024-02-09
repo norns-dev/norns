c1b355f3736db11d7e9b8464eb77acde87bbc8bd
Josh Odell <j01101111sh@gmail.com>
2024-02-09 15:35:40 -0600

    Added unique constraint on user email address

--- accounts/models.py
+++ accounts/models.py
@@ -13,7 +13,9 @@ class CustomUser(AbstractUser):
     location = models.CharField(max_length=50, null=True, blank=True)
     timezone = models.CharField(max_length=32, choices=TIMEZONES, default="UTC")
 
-    email = models.EmailField(blank=False, max_length=254, verbose_name="email address")
+    email = models.EmailField(
+        blank=False, max_length=254, verbose_name="email address", unique=True
+    )
 
     def __str__(self):
         return str(self.username)
