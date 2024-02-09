63f3f53f39150e88351a9086a94288b452d3587a
Josh Odell <j01101111sh@gmail.com>
2024-02-09 17:00:10 -0600

    Added user update view

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,12 @@
 Views for accounts app on norns
 """
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from .forms import CustomUserCreationForm
+from .models import CustomUser
 
 
 class SignUpView(CreateView):
@@ -14,3 +16,32 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class UserProfileView(LoginRequiredMixin, DetailView):
+    """User profile view"""
+
+    model = CustomUser
+
+    template_name = "registration/user_update_complete.html"
+
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(id=self.request.user.id)
+
+
+class UpdateUserView(UpdateView, LoginRequiredMixin):
+    """Update user view"""
+
+    model = CustomUser
+    fields = (
+        "email",
+        "first_name",
+        "last_name",
+        "location",
+        "timezone",
+    )
+    template_name = "registration/user_update.html"
+    success_url = reverse_lazy("user_update_complete", args=[1])
+
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(id=self.request.user.id)
