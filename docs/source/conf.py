37c77c1c2aa70b3478cd6b1783adcc50a36b7ba2
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2024-02-09 13:10:14 -0600

    Added documentation structure (#51)

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+import pathlib
+import sys
+
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "norns"
+copyright = "2024, Josh Odell"
+author = "Josh Odell"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+
+templates_path = ["_templates"]
+
+autodoc_member_order = "groupwise"
+autodoc_typehints = "description"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
