027dd74b71f2fbd46d5cff4e67e7769b1f68beaf
Josh Odell <j01101111sh@gmail.com>
2024-02-10 20:50:51 -0600

    Added additional tests for party list owned

--- parties/tests.py
+++ parties/tests.py
@@ -28,7 +28,7 @@ class PartyDetailPageTest(TestCase):
         cls.test_obj_id = party.id
         cls.abs_url = f"/parties/{cls.test_obj_id}/"
         cls.view_name = "party_detail"
-        cls.template_name = "parties/party_detail.html"
+        cls.expected_templates = ["base.html", "parties/party_detail.html"]
 
     def setUp(self) -> None:
         # Logs the test user in
@@ -46,10 +46,100 @@ class PartyDetailPageTest(TestCase):
 
     def test_template_name_correct(self):
         response = self.client.get(self.abs_url, follow=True)
-        self.assertTemplateUsed(response, "base.html")
-        self.assertTemplateUsed(response, self.template_name)
+        for template in self.expected_templates:
+            self.assertTemplateUsed(response, template)
 
     def test_login_required(self):
         self.client.logout()
         response = self.client.get(self.abs_url, follow=True)
-        self.assertTemplateNotUsed(response, self.template_name)
+        self.assertTemplateNotUsed(response, self.expected_templates[-1])
+
+
+class PartyListOwnedPageTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Create test user
+        cls.user_params = {"username": "testuser", "password": "testing123"}
+        cls.user = User.objects.create_user(**cls.user_params, email="test@localhost")
+        super().setUpClass()
+
+        # Create test Party
+        party = Party.objects.create(name="testparty", dm=cls.user)
+        party.save()
+
+        # Set reusable variables
+        cls.abs_url = f"/parties/owned/"
+        cls.view_name = "party_list_owned"
+        cls.expected_templates = [
+            "base.html",
+            "parties/party_list.html",
+            "parties/party_list_owned.html",
+        ]
+
+    def setUp(self) -> None:
+        # Logs the test user in
+        self.client.login(**self.user_params)
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get(self.abs_url, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_view_name(self):
+        response = self.client.get(reverse(self.view_name), follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(self.abs_url, follow=True)
+        for template in self.expected_templates:
+            self.assertTemplateUsed(response, template)
+
+    def test_login_required(self):
+        self.client.logout()
+        response = self.client.get(self.abs_url, follow=True)
+        self.assertTemplateNotUsed(response, self.expected_templates[-1])
+
+
+class PartyListOwnedPageTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Create test user
+        cls.user_params = {"username": "testuser", "password": "testing123"}
+        cls.user = User.objects.create_user(**cls.user_params, email="test@localhost")
+        super().setUpClass()
+
+        # Create test Party
+        party = Party.objects.create(name="testparty", dm=cls.user)
+        party.save()
+
+        # Set reusable variables
+        cls.abs_url = f"/parties/owned/"
+        cls.view_name = "party_list_owned"
+        cls.expected_templates = [
+            "base.html",
+            "parties/party_list.html",
+            "parties/party_list_owned.html",
+        ]
+
+    def setUp(self) -> None:
+        # Logs the test user in
+        self.client.login(**self.user_params)
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get(self.abs_url, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_view_name(self):
+        response = self.client.get(reverse(self.view_name), follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(self.abs_url, follow=True)
+        for template in self.expected_templates:
+            self.assertTemplateUsed(response, template)
+
+    def test_login_required(self):
+        self.client.logout()
+        response = self.client.get(self.abs_url, follow=True)
+        self.assertTemplateNotUsed(response, self.expected_templates[-1])
