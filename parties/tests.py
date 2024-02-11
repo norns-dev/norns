fd1cf8eb032dda378ab2e30d24feafa779f61f2e
Josh Odell <j01101111sh@gmail.com>
2024-02-10 20:52:27 -0600

    Added additional tests for party list joined

--- parties/tests.py
+++ parties/tests.py
@@ -143,3 +143,48 @@ class PartyListOwnedPageTest(TestCase):
         self.client.logout()
         response = self.client.get(self.abs_url, follow=True)
         self.assertTemplateNotUsed(response, self.expected_templates[-1])
+
+
+class PartyListJoinedPageTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Create test user
+        cls.user_params = {"username": "testuser", "password": "testing123"}
+        cls.user = User.objects.create_user(**cls.user_params, email="test@localhost")
+        super().setUpClass()
+
+        # Create test Party
+        party = Party.objects.create(name="testparty", dm=cls.user)
+        party.save()
+
+        # Set reusable variables
+        cls.abs_url = f"/parties/joined/"
+        cls.view_name = "party_list_joined"
+        cls.expected_templates = [
+            "base.html",
+            "parties/party_list.html",
+            "parties/party_list_joined.html",
+        ]
+
+    def setUp(self) -> None:
+        # Logs the test user in
+        self.client.login(**self.user_params)
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get(self.abs_url, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_view_name(self):
+        response = self.client.get(reverse(self.view_name), follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(self.abs_url, follow=True)
+        for template in self.expected_templates:
+            self.assertTemplateUsed(response, template)
+
+    def test_login_required(self):
+        self.client.logout()
+        response = self.client.get(self.abs_url, follow=True)
+        self.assertTemplateNotUsed(response, self.expected_templates[-1])
