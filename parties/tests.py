ea6c58f6817062e3f0e4dac65b126f2a6cebb948
Josh Odell <j01101111sh@gmail.com>
2024-02-10 22:49:41 -0600

    Added tests for party delete page

--- parties/tests.py
+++ parties/tests.py
@@ -322,3 +322,47 @@ class PartyEditPageTest(TestCase):
         self.client.logout()
         response = self.client.get(self.abs_url, follow=True)
         self.assertTemplateNotUsed(response, self.expected_templates[-1])
+
+
+class PartyDeletePageTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # Create test user
+        cls.user_params = {"username": "testuser6", "password": "testing123"}
+        cls.user = User.objects.create_user(**cls.user_params, email="test6@localhost")
+        super().setUpClass()
+
+        # Create test Party
+        party = Party.objects.create(name="testparty", dm=cls.user)
+        party.save()
+
+        # Set reusable variables
+        cls.test_obj_id = party.id
+        cls.abs_url = f"/parties/{cls.test_obj_id}/delete/"
+        cls.view_name = "party_delete"
+        cls.expected_templates = ["base.html", "parties/party_delete.html"]
+
+    def setUp(self) -> None:
+        # Logs the test user in
+        self.client.login(**self.user_params)
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get(self.abs_url, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_view_name(self):
+        response = self.client.get(
+            reverse(self.view_name, args=[self.test_obj_id]), follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(self.abs_url, follow=True)
+        for template in self.expected_templates:
+            self.assertTemplateUsed(response, template)
+
+    def test_login_required(self):
+        self.client.logout()
+        response = self.client.get(self.abs_url, follow=True)
+        self.assertTemplateNotUsed(response, self.expected_templates[-1])
