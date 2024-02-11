f6796aabfcc4891d930cfa93efa4af78197bb9ef
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2024-02-10 19:59:52 -0600

    Added user update view (#93)

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,10 @@
 
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, UpdateUserView, UserProfileView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("<int:pk>/edit/", UpdateUserView.as_view(), name="update_user"),
+    path("<int:pk>/", UserProfileView.as_view(), name="user_update_complete"),
 ]
