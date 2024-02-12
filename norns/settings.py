4fe564e484c9fd6e59f1c676664403edeedece48
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2024-02-12 14:05:02 -0600

    Added default secret key (#108)

--- norns/settings.py
+++ norns/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
 from environs import Env
 
 env = Env()
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = env.str("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", False)
