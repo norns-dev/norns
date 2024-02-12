bd699480462f283c58c1fcab47a3717a228373b7
Josh Odell <j01101111sh@gmail.com>
2024-02-12 14:03:51 -0600

    Using dj_db_url for setting db connection

--- norns/settings.py
+++ norns/settings.py
@@ -91,10 +91,7 @@ WSGI_APPLICATION = "norns.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.dj_db_url("DATABASE_URL", default="sqlite:///db.sqlite3"),
 }
 
 # Password validation
