123faad2391a93318c6fe5496fa0b4ce4214e4c5
Josh Odell <j01101111sh@gmail.com>
2024-02-12 15:07:41 -0600

    Cleaned up comments and settings file

--- norns/wsgi.py
+++ norns/wsgi.py
@@ -1,10 +1,5 @@
 """
 WSGI config for norns project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
