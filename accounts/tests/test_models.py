98feb9ba8373c7977f929138477ff2e68680d756
Josh Odell <j01101111sh@gmail.com>
2024-02-22 22:36:47 -0600

    Removed tests for now

--- accounts/tests/test_models.py
+++
@@ -1,8 +0,0 @@
-from django.test import SimpleTestCase
-
-from ..models import CustomUser
-
-
-class TestAccountsModels(SimpleTestCase):
-    def test_user_get_absolute_url(self, user: CustomUser):
-        self.assertEqual(user.get_absolute_url(), f"/users/{user.username}/")
