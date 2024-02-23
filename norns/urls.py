9cb5afcb376de879eb14e7ea9b9be0d249b4de42
Josh Odell <j01101111sh@gmail.com>
2024-02-22 23:05:59 -0600

    Added update view back

--- norns/urls.py
+++ norns/urls.py
@@ -8,6 +8,7 @@ from django.urls import include, path
 urlpatterns = [
     path("mstr/", admin.site.urls, name="admin"),
     path("accounts/", include("allauth.urls")),
+    path("accounts/", include("accounts.urls")),
     path("parties/", include("parties.urls")),
     path("", include("pages.urls")),
 ]
