7ecabb0761253a81ffcaf7203f2a7169a1b5bae2
Josh Odell <j01101111sh@gmail.com>
2024-02-22 22:56:44 -0600

    Removed old view

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,9 @@
 
 from django.urls import path
 
-from .views import SignUpView, UpdateUserView, UserProfileView
+from .views import UpdateUserView, UserProfileView
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
     path("<int:pk>/edit/", UpdateUserView.as_view(), name="update_user"),
     path("<int:pk>/", UserProfileView.as_view(), name="user_update_complete"),
 ]
