ac875bd4ba1f7b3324b5c797c9e360145afa6a95
Josh Odell <87207916+j01101111sh@users.noreply.github.com>
2024-02-26 21:14:07 -0600

    Added remove party member option for DMs (#151)
    Fixes #87

--- parties/urls.py
+++ parties/urls.py
@@ -9,12 +9,14 @@ from .views import (
     PartyListViewJoined,
     PartyListViewOwned,
     PartyUpdateView,
+    RemoveMemberView,
 )
 
 urlpatterns = [
     path("<int:pk>/", PartyDetailView.as_view(), name="party_detail"),
     path("<int:pk>/edit/", PartyUpdateView.as_view(), name="party_edit"),
     path("<int:pk>/delete/", PartyDeleteView.as_view(), name="party_delete"),
+    path("member_remove/<int:pk>/", RemoveMemberView.as_view(), name="member_remove"),
     path("new/", PartyCreateView.as_view(), name="party_new"),
     path("joined/", PartyListViewJoined.as_view(), name="party_list_joined"),
     path("owned/", PartyListViewOwned.as_view(), name="party_list_owned"),
